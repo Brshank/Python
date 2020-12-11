d9de875d25c2aaf504df4f6dce14b220b342b04a
Brshank Singh Negi <brsshanksn@gmail.com>
2020-12-12 03:29:40 +0530

    init

---
+++ 01_hello.py
@@ -0,0 +1,8 @@
+import os
+print('''Yeah
+I know sometimes things may not always make sense to you right now
+But hey, what daddy always tell you?
+Straighten up little soldier
+Stiffen up that upper lip
+What you crying about?
+You got me''') 
